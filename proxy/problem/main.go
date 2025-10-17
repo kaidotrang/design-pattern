0a4663cafa25930e5c1483633abe26837489edcc
kaidotrang <yeutrangbangan@gmail.com>
2025-10-17 12:26:58 +0700

    update proxy_problem

--- proxy/problem/main.go
+++ proxy/problem/main.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+// Đối tượng thật
+type Document struct {
+	Title   string
+	Content string
+}
+
+func (d *Document) Display() {
+	fmt.Printf("Tài liệu: %s\nNội dung: %s\n", d.Title, d.Content)
+}
+
+func main() {
+	doc := &Document{
+		Title:   "Kế hoạch tuyệt mật",
+		Content: "Chi tiết chiến lược...",
+	}
+
+	// Người dùng truy cập trực tiếp
+	doc.Display()
+}
